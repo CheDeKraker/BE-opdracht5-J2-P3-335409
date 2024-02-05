3883882475308b253a73c31e7fe3755590bf6ae0
BolleTito335409 <chemichaeldekraker@gmail.com>
2024-02-05 11:22:10 +0100

    database toegevoegd

--- app/config/config.php
+++ app/config/config.php
@@ -3,6 +3,6 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', '');
+define('DB_NAME', 'Opdracht_5');
 define('URLROOT', 'http://localhost:');
 define('PORT', '8000');//ONLY WORKS FOR LOCALHOST
